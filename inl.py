8955311952f03e24b6bbd1381f2d9aa7e2840a92
aMeijerr <caa.meijer@gmail.com>
2022-06-30 13:17:52 +0200

    update

--- inl.py
+++ inl.py
@@ -1,17 +1,19 @@
 import csv
 
+
 def read_file(file_name):
     with open(file_name, 'r') as f:
         data = csv.reader(f, delimiter=";")
         data_list = list(data)
         return data_list
 
-        
+
 def main():
     kameradata = read_file('kameraData.csv')
-    print(kameradata[0:3])
-    
+    print("Kameradata-filen:\n" + str(kameradata[0:3]))
+    print("\n")
     platsdata = read_file('platsData.csv')
     print("Platsdata-filen:\n" + str(platsdata[0:3]))
-          
+
+
 main()
